f554e99aa83af013298953f7d5d2a4a73dcdb73e
Kubas <kubasgc@icloud.com>
2018-04-24 13:02:45 +0200

    Usunięcie zbędnych paczek nuget, dodanie formularza logowania

---
+++ ProjektIO/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ProjektIO.Libraries;
+using ProjektIO.Models;
+
+namespace ProjektIO.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(Models.Account.LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            Uzytkownik user = new Uzytkownik() { Login = model.Login, Haslo = model.Password };
+
+            user = Auth.Repository.GetUserDetails(user);
+
+            if (user != null && user != default(Uzytkownik))
+            {
+                FormsAuthentication.SetAuthCookie(model.Login, false);
+
+                var authTicket = new FormsAuthenticationTicket(1, user.Login, DateTime.Now, DateTime.Now.AddMinutes(20), false, "");
+                string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+                var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                HttpContext.Response.Cookies.Add(authCookie);
+                return RedirectToAction("Index", "Home");
+
+            }
+
+            else
+            {
+                ModelState.AddModelError("", "Podano niepoprawne dane");
+                return View(model);
+            }
+        }
+    }
+}
---
+++ ProjektIO/Models/Account/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjektIO.Models.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Nazwa użytkownika")]
+        public string Login { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+}
