3c1981010dc63d995d73c3c31091f546660d67d9
Urszula Rumak <13crumak@gmail.com>
2018-05-01 21:58:18 +0200

    Proba umozliwienia edycji regulaminu

---
+++ ProjektIO/Controllers/LeaderController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjektIO.Models;
+
+namespace ProjektIO.Controllers
+{
+    public class LeaderController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Leader
+        //id w parametrze to id koła
+        public ActionResult EditStatute(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                KoloNaukowe group = new KoloNaukowe();
+                group = db.KoloNaukowe.FirstOrDefault(p => p.Id == id);
+                string statute = group.Regulamin;
+                return View(statute);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditStatute(int id, string statute)
+        {
+            using (var db = new DatabaseContext())
+            {
+                KoloNaukowe group = new KoloNaukowe();
+                group = db.KoloNaukowe.FirstOrDefault(p => p.Id == id);
+                if (ModelState.IsValid)
+                {
+                    group.Regulamin = statute;
+                    db.SaveChanges();
+                    TempData["message"] = string.Format("Zmiany zostały zapisane");
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(group.Regulamin);
+                }
+            }
+        }
+    }
+}
