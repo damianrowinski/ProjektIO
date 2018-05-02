4d314bcbc9084c217ef5d1f523c2e83c8825ad2d
Urszula Rumak <13crumak@gmail.com>
2018-05-02 20:05:37 +0200

    Dodawanie postow

---
+++ ProjektIO/Controllers/PostController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjektIO.Models;
+
+namespace ProjektIO.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //przekazuje id uzytkownika
+        public ActionResult AddPost(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                PostViewModel viewModel = new PostViewModel();
+                Czlonkowie member = db.Czlonkowie.Find(id);
+                viewModel.Member = member;
+                return View(viewModel);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddPost(PostViewModel viewModel)
+        {
+            using (var db = new DatabaseContext())
+            {
+                Post post = new Post();
+                post.IdCzlonka = viewModel.Member.Id;
+                post.Przypiety = false;
+                post.DataUtworzenia = DateTime.Now;
+                post.Zawartosc = viewModel.PostContent;
+                post.IdKola = viewModel.Member.IdKola;
+                db.Post.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ ProjektIO/Models/PostViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektIO.Models
+{
+    public class PostViewModel
+    {
+        public Czlonkowie Member { get; set; }
+        public string PostContent { get; set; }
+    }
+}
